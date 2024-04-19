a89280ce3f0bddf864c747e0cd6efd7825f7c65f
P-Ramos16 <p.ramos@ua.pt>
2024-04-19 12:01:27 +0100

    Added the initial repo state

---
+++ src/backend/src/main/java/deti/tqs/phihub/PhiHubApplication.java
@@ -0,0 +1,13 @@
+package deti.tqs.phihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhiHubApplication.class, args);
+	}
+
+}
---
+++ src/backend/src/test/java/deti/tqs/phihub/PhiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package deti.tqs.phihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
